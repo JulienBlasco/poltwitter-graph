1d1a87a6c74dc2b4081d1dfa36f7049bef8e64b2
Julien Blasco <jvdblasco@gmail.com>
2017-04-08 15:56:59 +0200

    First Commit

---
+++ code/my_config.py
@@ -0,0 +1,3 @@
+user_name = "topolitiq"
+user_id = "211124059"
+date = "2017-03-23"
---
+++ code/token_credentials.py
@@ -0,0 +1,200 @@
+import base64
+import requests
+import json
+
+proxies= {
+        }
+
+def token(proxy_exists, proxies=proxies):
+    b64_credentials = set()
+    
+    consumer_key = 'seITDT5BkGXh7Z5n5EEdUNOfU'
+    consumer_secret = 'ip4Xd6Ys4ojer17ZiCG9MS2RlYVo8pClNlxzmNyJ7suRRfqbUD'
+    access_token = '2980753832-VVq4275h9NxNWSirvd5kc4jhwIjFMPQlUQonvbx'
+    access_token_secret = 'OxenQNyM9PDVcwVRdWrmOKNHxWUud5yHJgikAiH8j8GR7'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = '0J4FYnyk57DTooCYR8zsctmvq'
+    consumer_secret = 'qGsF1Psue1QkQZyOEChtStHrFkIYGjP7wgSdIq9BO0d19STWqr'
+    access_token = '2980753832-qflITFY9x1cSqmYzVGb6KXNh7AYeKQ6tJffNihz'
+    access_token_secret = 'EMmtRWM9wNIQdURCueVykzCERgKxZAkLnHqPryt75UwYE'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'LLQM8Fl7XAWIotuogwEquYkFV'
+    consumer_secret = 'sN7zZXNzXSiM5N0cOiUPMKTdEuHG6BcbyjTENZpFXawHTZPgH0'
+    access_token = '2980753832-grb5IXejY6vjHFzUGTbO25ImVMmEoonE3OZYeyP'
+    access_token_secret = 'hkql2bHVjxqi2pjOLMfYy9Z50Bstxvjny5rVdBz4Wndew'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'iYFD3uQ5YJIDqX15hMXxJkj6z'
+    consumer_secret = '7Q4metqbErVBU2fYAMqWEXLlo52erwQyl07uUknzrBJ2G0g1Sb'
+    access_token = '2980753832-WAidgCsBZ1GdnsHdq8cNZpo3Q51liri9PK5Mr7R'
+    access_token_secret = 'vmzF7H2pFpgHUV1gGlpLSEuUn3yYup5sz9HozUy67q0hC'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'QeQJB0Jb22zIpCsJJ1CRwppWB'
+    consumer_secret = 'kePgckZNoYfX5ut2q11biAh1O6MXRS79HpKxgWSJ15B9FRoGxN'
+    access_token = '2980753832-awDAk8VMAzhKYSkM6RJ4aNYx1Ip5yD5H7eeVXCW'
+    access_token_secret = 'r7EdCfjRlKMATIRldBLIWBMCcXcH7p7bQ7aKNvOOztzpd'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'ZYuIFA8kPAKVg92BwtN84opkC'
+    consumer_secret = 'cdNW1lIfweJt1KnqQiGCQd8mp1bwSmTwDVVkzZbdUP10Yb9WmI'
+    access_token = '2980753832-VQKmIDH2NkanEDPCrcOuOOLt2PGjVd3f3zG1azm'
+    access_token_secret = 'wY0dgm60L27GaDuhFi877nbFgPkPNxQMbOZT8NYUwiUAB'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = '98Mv5H03IOEa4eXAn6P554qe9'
+    consumer_secret = 'x5OxOkOmLGwypkYa6i7RtXFUO1xQEQvAm7RWLE3BvfBxxN1H3S'
+    access_token = '2980753832-iRhNpxIFLkz7lGEdETqboZbVrfnfgnG6iAeCyd3'
+    access_token_secret = 'n75q0z4rdnLhCd5TiOEgaMkTHBiwtRhEzEddwxH0ZoduK'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = '98Mv5H03IOEa4eXAn6P554qe9'
+    consumer_secret = 'x5OxOkOmLGwypkYa6i7RtXFUO1xQEQvAm7RWLE3BvfBxxN1H3S'
+    access_token = '2980753832-iRhNpxIFLkz7lGEdETqboZbVrfnfgnG6iAeCyd3'
+    access_token_secret = 'n75q0z4rdnLhCd5TiOEgaMkTHBiwtRhEzEddwxH0ZoduK'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = '16D6V5JadSfdw2RSE8AvCssR8'
+    consumer_secret = 'h260S3YG6EII7ARBkEOzbF16UqejyouuLFgUFSrjRbCbNML2qa'
+    access_token = '2980753832-PrMKXaZ0HjzYxX1iq7xMLfeEFdLdejBfp0oyVrq'
+    access_token_secret = 'azZvsIZ0cMMwFgnv4PoPzmfzUnO2fsDNyN5M1UhGZyRjJ'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'seITDT5BkGXh7Z5n5EEdUNOfU'
+    consumer_secret = 'ip4Xd6Ys4ojer17ZiCG9MS2RlYVo8pClNlxzmNyJ7suRRfqbUD'
+    access_token = '2980753832-VVq4275h9NxNWSirvd5kc4jhwIjFMPQlUQonvbx'
+    access_token_secret = 'OxenQNyM9PDVcwVRdWrmOKNHxWUud5yHJgikAiH8j8GR7'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = '0J4FYnyk57DTooCYR8zsctmvq'
+    consumer_secret = 'qGsF1Psue1QkQZyOEChtStHrFkIYGjP7wgSdIq9BO0d19STWqr'
+    access_token = '2980753832-qflITFY9x1cSqmYzVGb6KXNh7AYeKQ6tJffNihz'
+    access_token_secret = 'EMmtRWM9wNIQdURCueVykzCERgKxZAkLnHqPryt75UwYE'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'LLQM8Fl7XAWIotuogwEquYkFV'
+    consumer_secret = 'sN7zZXNzXSiM5N0cOiUPMKTdEuHG6BcbyjTENZpFXawHTZPgH0'
+    access_token = '2980753832-grb5IXejY6vjHFzUGTbO25ImVMmEoonE3OZYeyP'
+    access_token_secret = 'hkql2bHVjxqi2pjOLMfYy9Z50Bstxvjny5rVdBz4Wndew'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'iYFD3uQ5YJIDqX15hMXxJkj6z'
+    consumer_secret = '7Q4metqbErVBU2fYAMqWEXLlo52erwQyl07uUknzrBJ2G0g1Sb'
+    access_token = '2980753832-WAidgCsBZ1GdnsHdq8cNZpo3Q51liri9PK5Mr7R'
+    access_token_secret = 'vmzF7H2pFpgHUV1gGlpLSEuUn3yYup5sz9HozUy67q0hC'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'seITDT5BkGXh7Z5n5EEdUNOfU'
+    consumer_secret = 'ip4Xd6Ys4ojer17ZiCG9MS2RlYVo8pClNlxzmNyJ7suRRfqbUD'
+    access_token = '2980753832-VVq4275h9NxNWSirvd5kc4jhwIjFMPQlUQonvbx'
+    access_token_secret = 'OxenQNyM9PDVcwVRdWrmOKNHxWUud5yHJgikAiH8j8GR7'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = '0J4FYnyk57DTooCYR8zsctmvq'
+    consumer_secret = 'qGsF1Psue1QkQZyOEChtStHrFkIYGjP7wgSdIq9BO0d19STWqr'
+    access_token = '2980753832-qflITFY9x1cSqmYzVGb6KXNh7AYeKQ6tJffNihz'
+    access_token_secret = 'EMmtRWM9wNIQdURCueVykzCERgKxZAkLnHqPryt75UwYE'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'LLQM8Fl7XAWIotuogwEquYkFV'
+    consumer_secret = 'sN7zZXNzXSiM5N0cOiUPMKTdEuHG6BcbyjTENZpFXawHTZPgH0'
+    access_token = '2980753832-grb5IXejY6vjHFzUGTbO25ImVMmEoonE3OZYeyP'
+    access_token_secret = 'hkql2bHVjxqi2pjOLMfYy9Z50Bstxvjny5rVdBz4Wndew'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'iYFD3uQ5YJIDqX15hMXxJkj6z'
+    consumer_secret = '7Q4metqbErVBU2fYAMqWEXLlo52erwQyl07uUknzrBJ2G0g1Sb'
+    access_token = '2980753832-WAidgCsBZ1GdnsHdq8cNZpo3Q51liri9PK5Mr7R'
+    access_token_secret = 'vmzF7H2pFpgHUV1gGlpLSEuUn3yYup5sz9HozUy67q0hC'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = '0g3XJov5rXIsGbgB5UJW3ygJl'
+    consumer_secret = 'JKnhWIWFA0bGQrhFxb0MMXXR9H6P29cjI7kqoKZGc8st9KqDoI'
+    access_token = '2980753832-21Eh7Qs9EeRUa56uo2dMWRPizAqxHmrST0ayjJ5'
+    access_token_secret = '5AUzlT1OGJ9Qg46WiqiqIvVa54hIBbzNMY6l6uYn14Vxj'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'UJ8lccc3PfrX2PtMMAAak4yhT'
+    consumer_secret = 'pfqfCY4tFwB4I2gGqOVAvgpEJIcWkax8bPmcJjOi2mgRDFAsqJ'
+    access_token = '2980753832-u7K1JZSrFpFcX8Y9LxIfloTGFouLYqElBsAIjSH'
+    access_token_secret = 'RbpV2vHhZIHpKAMzlPiggOnm3aKSxJW9RpbxSd5th0c6M'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'MEM0fKqnDHx6xiKQsH4H2MhtH'
+    consumer_secret = 'kpuHMZlnKd3SgLhxMSvAIzDgSJzwUyitFe8NnKauTkMzJRwhS7'
+    access_token = '2980753832-cGzIvPMiFlsx69Tb9b4SwduT95qw5wR08MZuHRb'
+    access_token_secret = 'TSYELTTLiQznDSAUFX7aNMrS75iuIBGsxttNhazyRGDEd'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'Gb8oikIfsETRii7nueRy3liJl'
+    consumer_secret = '4F1VyeHei27suwtQqS5CbmOgFAcAiJhHDNIjK0q9IfGf2ql0vG'
+    access_token = '2980753832-YGI9X4DzFYJ9XOS1nAZiRAHCjFREMShXjpzXU6M'
+    access_token_secret = 'VilNDj45jHFFlfdee832W5Lli1aK08zFJWa7eWUgkPPgM'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'EMCgYfVTBGtpVaj5NbW1IRujx'
+    consumer_secret = 'I2562HsqhoXWcoZ4MbSaNivymTsRhFRLhVXmL0AvHQc9yl1qK4'
+    access_token = '2980753832-wwXIdr9gRsXcQ9URQAkSWedJpx8n4auEBp1C5Ur'
+    access_token_secret = 'TUQWo6ozA9Cr1DX0MZLLGLeUhBwnzuc9XhiQrd36GYToA'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = '61mkrTiyjID4cype5cXmNxEbw'
+    consumer_secret = 'k6EjYFUx8cs9NnyGEiby3AJuASa9gtPR2ueiBQY1V2iZUiex50'
+    access_token = '2980753832-kD0zA893WAReKrTSyiSclogrOxv39QXL3X325aM'
+    access_token_secret = 'bTtsFvZ4VfmaPK25Df6uMuVi3gRWTapy4gMbbbiz5YNq1'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'w0jBhAtn7ghZxpwffMOx8UYkk'
+    consumer_secret = 'PTZHjZU0YZnRxQ02FBdp8fUkY8lqFwir6AhC4lXAE3XEO9VWHl'
+    access_token = '2980753832-KFEFjZvDlugdEVMhLcN228Aj5sM8sFpHpFD7VcY'
+    access_token_secret = '8jnq88qBxSh6gpLZJt1ocGE9jNo7Qh6Eq6100t75r6cNg'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+
+    consumer_key = 'FYfDXndEMS057EI1CfF3sQ8Sd'
+    consumer_secret = 'PojVxzYZz28vwtNzlgUwUtfPlmFaeY9VDmccIr6pT9ofriGPDx'
+    access_token = '2980753832-brxnWiMw06IqK0ZaGU9dGtsqsZJyiewnWTUIJYE'
+    access_token_secret = 'BCDyzg6s5CIiLaY3CGGuxAXaOOucXgbf0Oc9t1cKPYB4l'
+
+    b64_credentials.add(base64.b64encode(bytes(consumer_key + ":" + consumer_secret,'utf-8')))
+    
+    bearer_token = []
+    for credential in b64_credentials:
+        r = requests.post(
+            "https://api.twitter.com/oauth2/token",
+            params={"grant_type" : "client_credentials"},
+            headers={
+                "Authorization" : "Basic "+credential.decode('utf-8'),
+                "Content-Type" : "application/x-www-form-urlencoded;charset=UTF-8"
+            },
+            proxies=proxies if proxy_exists else {}
+        )
+
+        bearer_token.append(json.loads(r.text)["access_token"])
+        
+    return(bearer_token)
