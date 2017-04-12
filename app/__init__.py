f0a24ec351aace0c0f36b7690e3229d710492e41
JulienBlasco <jvdblasco@gmail.com>
2017-04-12 10:51:27 +0200

    remove old imports

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,14 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 
-from PIL import Image
 from io import BytesIO
 from flask import Flask, render_template, make_response, redirect, url_for
-from flask_socketio import SocketIO
+# from flask_socketio import SocketIO
 from datetime import date
 
 app = Flask(__name__)
-socketio = SocketIO()
-socketio.init_app(app)
-
-
-@app.context_processor
-def passer_ingredient():
-    return dict(ingredient="caramel")
+# socketio = SocketIO()
+# socketio.init_app(app)
 
 
 @app.route("/")
@@ -22,27 +16,10 @@ def blank():
     return render_template("index.html")
 
 
-@app.route('/Bienvenue/<nom>')
-def accueil(nom="visiteur"):
-    d = date.today().isoformat()
-    mots = ["bonjour", "à", "toi,", nom+"."]
-    return render_template('accueil.html', titre="Bienvenue !", mots=mots, date=d)
-
-
 @app.route("/<page>")
 @app.route("/<page>.html")
 def specific_page(page):
     return render_template(page+".html")
-
-
-@app.route('/image')
-def genere_image():
-    mon_image = BytesIO()
-    Image.new("RGB", (300,300), "#92C41D").save(mon_image, 'BMP')
-    reponse = make_response(mon_image.getvalue(),404)
-    reponse.mimetype = "image/bmp"
-    return reponse
-    #return redirect(url_for("accueil", nom="Jean-Michel"))
 
 
 @app.errorhandler(404)
