6cbf5b776b61fe8bbba5af07f5c0b2066a69de45
JulienBlasco <jvdblasco@gmail.com>
2017-04-24 20:03:02 +0200

    moved scripts to my_script

--- app/static/js/my_script.js
+++ app/static/js/my_script.js
@@ -1,4 +1,57 @@
 /**
+** BORDER AROUND NODES
+*/
+
+    ;(function(undefined) {
+
+  /**
+   * Sigma Node Border Custom Renderer
+   * ==================================
+   *
+   * The aim of this simple node renderer is to enable the user to display
+   * colored node borders.
+   *
+   * Author: Guillaume Plique (Yomguithereal)
+   * Version: 0.0.1
+   */
+
+  sigma.canvas.nodes.border = function(node, context, settings) {
+    var prefix = settings('prefix') || '';
+
+    context.fillStyle = node.color || settings('defaultNodeColor');
+    context.beginPath();
+    context.arc(
+     node[prefix + 'x'],
+     node[prefix + 'y'],
+     node[prefix + 'size'],
+     0,
+     Math.PI * 2,
+     true
+    );
+
+    context.closePath();
+    context.fill();
+
+    context.lineWidth = node.borderWidth || 1;
+    context.strokeStyle = '#fff';
+    context.stroke();
+  };
+}).call(this);
+
+// Add a method to the graph model that returns an
+// object with every neighbors of a node inside:
+  sigma.classes.graph.addMethod('neighbors', function(nodeId) {
+    var k,
+        neighbors = {},
+        index = this.allNeighborsIndex[nodeId] || {};
+
+    for (k in index)
+      neighbors[k] = this.nodesIndex[k];
+
+    return neighbors;
+  });
+
+/**
 * ON GRAPH LOADING
 */
 function implement_clickfunction(s) {
